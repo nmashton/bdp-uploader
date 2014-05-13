c0f8c09018f5a4a91f33b0ac73f08bc396a39c1b
Tryggvi Bjorgvinsson <tryggvi.bjorgvinsson@okfn.org>
2014-05-13 12:03:50 +0000

    Configure the app directly from the config file
    Flask can configure itself directly from the config file instead
    of importing everything from it and explicitly setting it to a
    configuration variable with the same name.
    
    So instead of:
    
        from app.config import UPLOADS
        app.config['UPLOADS'] = UPLOADS
    
    we can do:
    
        app.config.from_object('app.config')

--- app/bdp.py
+++ app/bdp.py
@@ -1,13 +1,8 @@
 from flask import Flask
-from .config import UPLOADS, METADATA, DATA, ZIPS, EXTENSIONS, DEBUG
 
 # Create the Flask application
 app = Flask(__name__)
 
 # Configure the Flask app based on configurations from the config file
-app.config["UPLOADS"] = UPLOADS
-app.config["METADATA"] = METADATA
-app.config["DATA"] = DATA
-app.config["ZIPS"] = ZIPS
-app.config["EXTENSIONS"] = EXTENSIONS
-app.debug = DEBUG
+app.config.from_object('app.config')
+
